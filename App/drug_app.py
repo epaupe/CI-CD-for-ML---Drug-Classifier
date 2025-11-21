413cbcd33ad404702e61822b52d531af353fa267
epaupe <epaupe@ethz.ch>
2025-11-21 14:37:17 +0100

    final

--- App/drug_app.py
+++ App/drug_app.py
@@ -59,7 +59,7 @@ examples = [
 
 title = "Drug Classification"
 description = "Enter the details to correctly identify Drug type?"
-article = "This app is a part of the **[Beginner's Guide to CI/CD for Machine Learning](https://www.datacamp.com/tutorial/ci-cd-for-machine-learning)**. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
+
 
 
 gr.Interface(
@@ -69,6 +69,5 @@ gr.Interface(
     examples=examples,
     title=title,
     description=description,
-    article=article,
     theme=gr.themes.Soft(),
 ).launch()
